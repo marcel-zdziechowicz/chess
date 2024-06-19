a747754ccbd56c59edd53a9c00701cce40d2334a
marcel-zdziechowicz <marcel.zdziechowiczm@gmail.com>
2024-06-19 19:33:51 +0200

    Add handling command line arguments

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,6 +1,7 @@
 #include "Application.hpp"
 #include <vector>
 #include <iostream>
+#include <iomanip>
 
 namespace Chess {
 
@@ -12,8 +13,14 @@ void PrintHelp()
   //   --help                       Display help
   std::cout << "Usage: chess [options]\n\n";
   std::cout << "Options:\n";
-  std::cout << "\t--mode {console | window}\n";
-  std::cout << "\t--help\n";
+
+  std::cout << std::setw(26) 
+            << "\t--mode {console | window}"
+            << "\tSets the mode for opening chess\n";
+
+  std::cout << std::setw(26) 
+            << "\t--help"
+            << "\tDisplay help\n";
 }
 
 ApplicationBase* EvalCMDArguments(int argc, char** argv)
@@ -21,8 +28,23 @@ ApplicationBase* EvalCMDArguments(int argc, char** argv)
   std::vector<std::string> arguments(argc);
   for (int i = 0; i < argc; i++) {
     arguments.push_back(argv[i]);
-    std::cout << arguments.back() << '\n';
   }
+  if (arguments.size() == 1) {
+    return new WindowApplication;
+  }
+  else if (arguments.size() == 3) {
+    if (arguments[2] == "window") {
+      return new WindowApplication;
+    }
+    else if (arguments[2] == "console") {
+      return new ConsoleApplication;
+    }
+    else {
+      PrintHelp();
+      return nullptr;
+    }
+  }
+  PrintHelp();
   return nullptr;
 }
 
